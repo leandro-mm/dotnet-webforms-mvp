ffd7d7e6a354bd1728ca8f48760a0e13b68bfcdd
Leandro M Machado <leandro-m-m@hotmail.com>
2026-05-18 15:43:55 -0300

    implementing Movie Management lntegration Tests

--- MovieManager.Tests/Models/MovieTests.cs
+++ MovieManager.Tests/Models/MovieTests.cs
@@ -2,11 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.IO;
+
 using System.Linq;
-using System.Runtime.Remoting.Contexts;
-using System.Text;
-using System.Threading.Tasks;
+
 using WebForms_MovieManager.Models;
 
 namespace MovieManager.Tests.Models
---
+++ MovieManager.Tests/lntegrationTests/MovieManagementlntegrationTests.cs
@@ -0,0 +1,91 @@
+﻿using FluentAssertions;
+using MovieManager.Tests.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebForms_MovieManager.Models;
+using WebForms_MovieManager.Presenters;
+using WebForms_MovieManager.Repositories;
+
+namespace MovieManager.Tests.lntegrationTests
+{
+    public class MovieManagementlntegrationTests
+    {
+        [Fact]
+        public void CompleteMovieWorkflow_ShouldWorkCorrectly()
+        {
+            //Arrange
+            var view = new MockView();
+            var repository = new MovieRepository();
+            var presenter = new MoviePresenter(view, repository);
+
+            view.MovieTitle = "Integration Test Movie";
+            view.Director = "Test Director";
+            view.ReleaseYear = DateTime.Now.Year.ToString();
+            view.Genre = "Test Genre";
+            view.Rating = "9";
+            view.RaiseAddMovieEvent();
+
+            //Assert - Movie was added
+            view.SuccessMessage.Should().Contain("successfully");
+
+            //Act - Load movies
+            view.RaiseLoadMovieEvent();
+
+            //Assert - Movie appears in grid
+            view.MoviesDataSource.Should().Contain(m => m.MovieTitle == "Integration Test Movie");
+            var addedMovie = view.MoviesDataSource.First(m => m.MovieTitle == "Integration Test Movie");
+
+            //Act - Edit the movie
+            view.MovieId = addedMovie.Id.ToString();
+            view.RaiseEditMovieEvent();
+
+            //Assert - Form popuLated for editing
+            view.MovieTitle.Should().Be("Integration Test Movie");
+            view.IsFormEditMode.Should().BeTrue();
+
+            //Act - Update the movie
+            view.MovieTitle = "Updated Integration Movie";
+            view.Rating = "9";
+            view.RaiseUpdateMovieEvent();
+
+            //Assert - Movie was updated
+            view.SuccessMessage.Should().Contain("update");
+            view.RaiseLoadMovieEvent();
+            view.MoviesDataSource.Should().Contain(m => m.MovieTitle == "Updated Integration Movie");
+
+            view.MovieId = addedMovie.Id.ToString();
+            view.RaiseDeleteMovieEvent();
+
+            //Assert - Movie was deLeted
+            view.SuccessMessage.Should().Contain("deleted");
+            view.RaiseLoadMovieEvent();
+            view.MoviesDataSource.Should().NotContain(m => m.Id == addedMovie.Id);
+
+        }
+
+        [Fact]
+        public void ValidationErrors_ShouldPreventInvalidOperations()
+        {
+            //Arrange
+            var view = new MockView();
+            var repository = new MovieRepository();
+            var presenter = new MoviePresenter(view, repository);
+            var initialCount = repository.GetAllMovies().Count();
+
+            view.MovieTitle = "";
+            view.Director = "Test Director";
+            view.ReleaseYear = DateTime.Now.Year.ToString();
+            view.Genre = "Test Genre";
+            view.Rating = "9";
+            view.RaiseAddMovieEvent();
+
+            //Assert - Movie was added
+            view.ValidationErrors.Should().NotBeEmpty();
+            repository.GetAllMovies().Count().Should().Be(initialCount);
+
+        }
+    }
+}
